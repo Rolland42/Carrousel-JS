79b94f5be154ce684bcf0b0ae3a141d3b54933cc
sonia <s.rolland@codeur.online>
2021-04-28 11:53:42 +0200

    two

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrousel-Js</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    
+<div id="carrousel"></div>
+<button id="btn-left">left</div>
+<button id="btn-right">right</div>
+
+<script src="carrousel.js"></script>
+
+</body>
+</html>
